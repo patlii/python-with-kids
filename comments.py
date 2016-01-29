c9bca65d5a02512d62f9de471e466527e9b142fa
patlii <patli@qq.com>
2016-01-29 11:19:49 +0800

    Add comments.py

---
+++ comments.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# This code line is a comment.
+# This code line is a comment too.
+# The comment lines will be ignored.
+# The comment lines are visible for human.
+# The comment lines are invisible for computer.
+
+print('hello world');
